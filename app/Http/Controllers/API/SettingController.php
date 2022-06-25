fc1f886bbfe5c6184a7c51600a3cce0ab9caa9ba
tn-darkcode <tn.darkcode@gmail.com>
2022-06-26 01:14:28 +0200

    -Merchants can decide if the VAT is included in the products price

---
+++ app/Http/Controllers/API/SettingController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Setting;
+
+class SettingController extends Controller
+{
+    public function store(Request $request)
+    {
+        
+        $authUser = auth()->user();
+        $setting = Setting::create([
+            'VAT_included' => $request->VAT_included,
+            'VAT' => $request->VAT,
+            'store_id' => $authUser->store_id
+        ]);
+    
+        return response()->json(['created' => 'created'], 200);
+    }
+}
---
+++ database/migrations/2022_06_25_224654_create_settings_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSettingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('settings', function (Blueprint $table) {
+            $table->id();
+            $table->integer('VAT_included')->default(0); //1 means VAT included in the products price
+            $table->integer('VAT')->nullable();
+            $table->integer('store_id')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('settings');
+    }
+}
